2cb83b8b67f97b7539c56392ad7b9d20ab85d672
Mikael Mortensen <mikaem@math.uio.no>
2024-08-23 21:07:23 +0200

    Update setupenv.sh

--- .devcontainer/setupenv.sh
+++ .devcontainer/setupenv.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo 'pyver=$(python -c "import sys;print(sys.version[:(len(str(sys.version_info.major))+len(str(sys.version_info.minor))+1)])")' >> ~/.bashrc
 echo 'source activate l2copenblas' >> ~/.bashrc
-echo 'export PATH=/workspaces/Legendre-to-Chebyshev/src/build-install/bin:$PATH' >> ~/.bashrc
-echo 'export LD_LIBRARY_PATH=/workspaces/Legendre-to-Chebyshev/src/build-install/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
-echo 'export PYTHONPATH=/workspaces/Legendre-to-Chebyshev/src/build-install/lib/python${pyver}/site-packages:$PYTHONPATH' >> ~/.bashrc
-source ~/.bashrc
+echo 'export PATH=/workspaces/SISC-Legendre-to-Chebyshev/src/build-install/bin:$PATH' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=/workspaces/SISC-Legendre-to-Chebyshev/src/build-install/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
+echo 'export PYTHONPATH=/workspaces/SISC-Legendre-to-Chebyshev/src/build-install/lib/python${pyver}/site-packages:$PYTHONPATH' >> ~/.bashrc
